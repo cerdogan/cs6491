c4d64b0108edc62dce232f6abfe862ecdd09436d
cerdogan <cerdogan3@gatech.edu>
2015-09-26 11:40:52 -0400

    Apparent bug in rotation computations. Maybe, hopefully, alpha computation is correct now

--- matlab/euler.m
+++ matlab/euler.m
@@ -5,20 +5,20 @@
 %% returns the rotation matrix.
 
 function R = euler (th1, th2, th3)
-  R = rotz(th1 / pi * 180) * roty(th2 / pi * 180) * rotx(th3 / pi * 180);
+  R = rotz_2(th1) * roty_2(th2) * rotx_2(th3);
 end
 
-function R = rotx(th)
+function R = rotx_2(th)
   c = cos(th); s = sin(th);
   R = [1 0 0; 0 c -s; 0 s c];
 end
 
-function R = roty(th)
+function R = roty_2(th)
   c = cos(th); s = sin(th);
   R = [c 0 s; 0 1 0; -s 0 c];
 end
 
-function R = rotz(th)
+function R = rotz_2(th)
   c = cos(th); s = sin(th);
   R = [c -s 0; s c 0; 0 0 1];
 end
